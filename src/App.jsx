8b9a6028db7994e85765a12acb8b0f65e7a5e443
Goutham Korla <goutham.korla@infor.com>
2025-10-17 11:18:44 +0530

    Configured Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div className='w-50 mx-auto my-7 p-4 bg-green-600 rounded-2xl border-red-400 border-2 '>
+      Cart
+    </div>
+  )
+}
+
+export default App
